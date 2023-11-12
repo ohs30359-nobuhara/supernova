c9bfaad6788d04ee5bad66946aae5c1b6d38a138
信原 有志 <ynobuhar@yahoo-corp.jp>
2023-11-12 16:43:37 +0900

    基本的なcurl処理を実装

--- template/curl.go
+++ template/curl.go
@@ -1,6 +1,9 @@
 package template
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+)
 
 type CurlTemplate struct {
 	URL    string   `yaml:"url"`
@@ -10,6 +13,27 @@ type CurlTemplate struct {
 	Cookie string   `yaml:"cookie"`
 }
 
-func (t CurlTemplate) Run() {
-	fmt.Println("aaa")
+func (t CurlTemplate) Run() error {
+	req, e := http.NewRequest(t.Method, t.URL, nil)
+	if e != nil {
+		return e
+	}
+
+	for _, h := range t.Header {
+		parts := strings.Split(h, ":")
+		if len(parts) == 2 {
+			req.Header.Set(strings.TrimSpace(parts[0]), strings.TrimSpace(parts[1]))
+		}
+	}
+
+	req.Header.Set("Cookie", t.Cookie)
+
+	client := http.Client{}
+	resp, e := client.Do(req)
+	if e != nil {
+		return e
+	}
+	defer resp.Body.Close()
+
+	return nil
 }
